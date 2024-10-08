285222915d727b81dea3fbad62103cc3228cb3d1
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-10-08 15:23:22 -0500

    Small update

--- display_message_file.py
+++ display_message_file.py
@@ -1,29 +1,77 @@
 #!/usr/bin/env python
 
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
 import argparse
 import sys
-from parse_message_file import (
-    parse_file, display_services, to_json, from_json, to_xml, from_xml,
-    load_from_json_file, save_to_json_file, load_from_xml_file, save_to_xml_file,
-    services_to_string, save_services_to_file
-)
+
+from parse_message_file import (display_services, from_json, from_xml,
+                                load_from_json_file, load_from_xml_file,
+                                parse_file, save_services_to_file,
+                                save_to_json_file, save_to_xml_file,
+                                services_to_string, to_json, to_xml)
+
 
 def main():
-    parser = argparse.ArgumentParser(description="Parse and display message file content.")
+    parser = argparse.ArgumentParser(
+        description="Parse and display message file content.")
     parser.add_argument("filename", help="Path to the file to be parsed")
-    parser.add_argument("--validate-only", "-v", action="store_true", help="Only validate the file without displaying")
-    parser.add_argument("--verbose", "-V", action="store_true", help="Enable verbose mode")
-    parser.add_argument("--debug", "-d", action="store_true", help="Enable debug mode")
-    parser.add_argument("--to-json", "-j", help="Convert the parsed data to JSON and save to a file")
-    parser.add_argument("--from-json", "-J", help="Load the services data structure from a JSON file")
-    parser.add_argument("--json-string", "-s", type=str, help="JSON string to parse if --from-json is specified")
-    parser.add_argument("--to-xml", "-x", help="Convert the parsed data to XML and save to a file")
-    parser.add_argument("--from-xml", "-X", help="Load the services data structure from an XML file")
-    parser.add_argument("--xml-string", "-S", type=str, help="XML string to parse if --from-xml is specified")
-    parser.add_argument("--to-original", "-o", help="Convert the parsed data back to the original format and save to a file")
-    parser.add_argument("--line-ending", "-l", choices=["lf", "cr", "crlf"], default="lf", help="Specify the line ending format for the output file")
-    
+    parser.add_argument(
+        "--validate-only",
+        "-v",
+        action="store_true",
+        help="Only validate the file without displaying")
+    parser.add_argument(
+        "--verbose",
+        "-V",
+        action="store_true",
+        help="Enable verbose mode")
+    parser.add_argument(
+        "--debug",
+        "-d",
+        action="store_true",
+        help="Enable debug mode")
+    parser.add_argument(
+        "--to-json",
+        "-j",
+        help="Convert the parsed data to JSON and save to a file")
+    parser.add_argument(
+        "--from-json",
+        "-J",
+        help="Load the services data structure from a JSON file")
+    parser.add_argument(
+        "--json-string",
+        "-s",
+        type=str,
+        help="JSON string to parse if --from-json is specified")
+    parser.add_argument(
+        "--to-xml",
+        "-x",
+        help="Convert the parsed data to XML and save to a file")
+    parser.add_argument(
+        "--from-xml",
+        "-X",
+        help="Load the services data structure from an XML file")
+    parser.add_argument(
+        "--xml-string",
+        "-S",
+        type=str,
+        help="XML string to parse if --from-xml is specified")
+    parser.add_argument(
+        "--to-original",
+        "-o",
+        help="Convert the parsed data back to the original format and save to a file")
+    parser.add_argument(
+        "--line-ending",
+        "-l",
+        choices=[
+            "lf",
+            "cr",
+            "crlf"],
+        default="lf",
+        help="Specify the line ending format for the output file")
+
     args = parser.parse_args()
 
     try:
@@ -41,7 +89,8 @@ def main():
             display_services(services)
         else:
             if args.validate_only:
-                is_valid, error_message, error_line = parse_file(args.filename, validate_only=True, verbose=args.verbose)
+                is_valid, error_message, error_line = parse_file(
+                    args.filename, validate_only=True, verbose=args.verbose)
                 if is_valid:
                     print("The file '{0}' is valid.".format(args.filename))
                 else:
@@ -50,7 +99,8 @@ def main():
             else:
                 services = parse_file(args.filename, verbose=args.verbose)
                 if args.debug:
-                    import pdb; pdb.set_trace()
+                    import pdb
+                    pdb.set_trace()
                 if args.to_json:
                     save_to_json_file(services, args.to_json)
                     print("Saved JSON to {0}".format(args.to_json))
@@ -58,13 +108,17 @@ def main():
                     save_to_xml_file(services, args.to_xml)
                     print("Saved XML to {0}".format(args.to_xml))
                 elif args.to_original:
-                    save_services_to_file(services, args.to_original, line_ending=args.line_ending)
-                    print("Saved original format to {0}".format(args.to_original))
+                    save_services_to_file(
+                        services, args.to_original, line_ending=args.line_ending)
+                    print(
+                        "Saved original format to {0}".format(
+                            args.to_original))
                 else:
                     display_services(services)
     except Exception as e:
         print("An error occurred: {0}".format(e), file=sys.stderr)
         sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
