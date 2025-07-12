c1a5e78ebca896d9f17b647f759ac5a2468b5cd8
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2025-07-11 20:17:07 -0500

    Update parse_message_file.py

--- parse_message_file.py
+++ parse_message_file.py
@@ -131,8 +131,8 @@ except NameError:
 __program_name__ = "LoveSoStrong";
 __project__ = __program_name__;
 __project_url__ = "https://repo.or.cz/LoveSoStrong.git";
-__version_info__ = (0, 0, 1, "RC 1", 1);
-__version_date_info__ = (2024, 8, 14, "RC 1", 1);
+__version_info__ = (0, 0, 5, "RC 1", 1);
+__version_date_info__ = (2025, 7, 11, "RC 1", 1);
 __version_date__ = str(__version_date_info__[0]) + "." + str(__version_date_info__[1]).zfill(2) + "." + str(__version_date_info__[2]).zfill(2);
 __revision__ = __version_info__[3];
 __revision_id__ = "$Id$";
