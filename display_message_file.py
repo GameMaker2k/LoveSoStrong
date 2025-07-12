b3ff94b6854211ab295703d31a193d9160c5997f
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2025-07-11 19:52:59 -0500

    Update display_message_file.py

--- display_message_file.py
+++ display_message_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals, generators, with_statement, nested_scopes
 import argparse
 import sys
 from parse_message_file import (
