05340532a5c4fa1fdbd4b1eb3fedd63cc177b40b
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2025-07-23 23:36:33 -0500

    Update __init__.py

--- pytextarchive/__init__.py
+++ pytextarchive/__init__.py
@@ -4,3 +4,4 @@
 from __future__ import absolute_import, division, print_function, unicode_literals, generators, with_statement, nested_scopes
 import pytextarchive.parse_message_file
 import pytextarchive.archive_parser
+import pytextarchive.mini_parser
